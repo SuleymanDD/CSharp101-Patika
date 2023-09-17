d494f1ec3ea94cdc4d8936235f2a1dba77cb45e5
Süleyman Demirel Devlet <sdemirel.2375@gmail.com>
2023-09-17 23:40:10 +0300

    enum-tanimi added

---
+++ enum-tanimi/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace enum_tanimi;
+class Program
+{
+    // NOTES
+	// Sıralı verilerde kullanılıyor, ardışık indexlerde
+	static void Main(string[] args)
+	{
+		Console.WriteLine(Gunler.Pazar);
+		Console.WriteLine((int)Gunler.Cumartesi);
+
+		int sicaklik = 32;
+
+		if (sicaklik <= (int)Hava_Durumu.Normal)
+			Console.WriteLine("Dişariya çikmak için havanin biraz daha isinmasini bekleyelim");
+		else if (sicaklik >= (int)Hava_Durumu.Sicak)
+			Console.WriteLine("Dişariya çikmak için çok sicak bir gün");
+		else if (sicaklik >= (int)Hava_Durumu.Normal && sicaklik <= (int)Hava_Durumu.Cok_Sicak)
+			Console.WriteLine("Hadi dişariya çikalim");
+	}
+	
+}
+enum Gunler
+{
+	Pazartesi = 1,
+	Sali, 
+	Çarşamba, 
+	Perşembe,
+	Cuma, 
+	Cumartesi,
+	Pazar
+}
+enum Hava_Durumu
+{
+	Soguk = 5,
+	Normal = 20,
+	Sicak = 25,
+	Cok_Sicak = 30
+}
